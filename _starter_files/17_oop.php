c232cfda82659734c28553025769bef6bed304e2
OybekJP <oybektoyo@gmail.com>
2023-01-02 21:03:57 +0500

    created class object in php

--- _starter_files/17_oop.php
+++ _starter_files/17_oop.php
@@ -4,3 +4,10 @@
 /*
   From PHP5 onwards you can write PHP in either a procedural or object oriented way. OOP consists of classes that can hold "properties" and "methods". Objects can be created from classes.
 */
+class Animal {
+  public $name;
+  public $type;
+  public $behaviour;
+
+
+}
