7b924b819f525cd870c1878b0ec3a02abd946354
amine1949 <74397650+amine1949@users.noreply.github.com>
2023-04-20 15:00:38 +0000

    Update 16_exceptions.php

--- 16_exceptions.php
+++ 16_exceptions.php
@@ -3,7 +3,9 @@
 /* ----------- Exceptions ----------- */
 
 /*
-  PHP has an exception model similar to that of other programming languages. An exception can be thrown, and caught ("catched") within PHP. Code may be surrounded in a try block, to facilitate the catching of potential exceptions. Each try must have at least one corresponding catch or finally block.
+  PHP has an exception model similar to that of other programming languages.
+  An exception can be thrown, and caught ("catched") within PHP. Code may be surrounded in a try block, to facilitate the catching of potential exceptions. 
+  Each try must have at least one corresponding catch or finally block.
 */
 
 function inverse($x) {
