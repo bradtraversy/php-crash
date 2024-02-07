e415ab5c1f51c97953b4ba60b4e2e378cf9e84b2
Uros <urosivanovich@gmail.com>
2024-02-07 00:18:47 -0500

    initial commit

--- _starter_files/01_output.php
+++ _starter_files/01_output.php
@@ -1,6 +1,15 @@
 <?php
 /* -------- Output & Comments ------- */
 
+// echo - Output string, numbers, html, etc
+// echo 123, 'Hello', 10.5;
+
+// print - Works like echo, but can only take in a single argument
+// print 123;
+
+// print_r() - Print single values and arrays
+print_r([1, 2, 3]);
+
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
