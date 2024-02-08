f6ab51684eccbd3fa74d625b0d06bf04f952f0e7
Uros <urosivanovich@gmail.com>
2024-02-07 22:38:53 -0500

    Arrays

--- _starter_files/03_arrays.php
+++ _starter_files/03_arrays.php
@@ -4,3 +4,64 @@
 /*
   Arrays hold multiple values. Each value in an array is called an "element"
 */
+// Simple Array
+$numbers = [1,44,55,22];
+$fruits = array('apple', 'orange', 'pear');
+
+// var_dump($numbers);
+// echo $fruits[1];
+
+// Associative Array
+$colors = [
+  1 => 'red',
+  4 => 'blue',
+  6 => 'green'
+];
+
+// echo $colors[4];
+
+$hex = [
+  'red' => '#f00',
+  'blue' => '#0f0',
+  'green' => '#00f'
+];
+
+// echo $hex['blue'];
+
+$person = [
+  'first_name' => 'Brad',
+  'last_name' => 'Traversy',
+  'email' => 'brand@gmail.com' 
+];
+
+// echo $person['first_name'];
+
+// var_dump($person);
+
+// Array of people
+$people = [
+  $person, //   [...$person1]
+  [
+    'first_name' => 'John',
+    'last_name' => 'Doe',
+    'email' => 'john@gmail.com',
+  ],
+  [
+    'first_name' => 'Jane',
+    'last_name' => 'Doe',
+    'email' => 'jane@gmail.com',
+  ],
+];
+
+// var_dump($people);
+
+// Accessing values in a multi-dimensional array
+// echo $people[0]['first_name'];
+// echo $people[2]['email'];
+
+// Encode to JSON
+var_dump(json_encode($people));
+
+// Decode from JSON
+$jsonobj = '{"first_name":"Brad","last_name": "Traversy","email":"brad@gmail.com"}';
+var_dump(json_decode($jsonobj));
