7eeb5f445ca2e43f7d22e10ad6d53752e0e2a6f2
Uros <urosivanovich@gmail.com>
2024-02-07 22:10:18 -0500

    debugging tools

--- _starter_files/01_output.php
+++ _starter_files/01_output.php
@@ -8,8 +8,36 @@
 // print 123;
 
 // print_r() - Print single values and arrays
-print_r([1, 2, 3]);
+// print_r([1, 2, 3]);
+
+// var_dump() - Returns more info like data type and length
+// var_dump('Hello');
+
+// var_export() - Similar var_dump(). Outputs a string representation of a variable
+// var_export('Hello');
+
+// Code above usually used for debugging
 
 /*
   A .php file running on a server, can output both HTML and PHP code. There are multiple functions that can be used to output data to the browser.
 */
+
+
+/*
+  <?=  ?> can be used instead of <?php echo ?> 
+  MUST USE SEMICOLONS!
+*/  
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <h1><?= 'Sha Sha';?></h1>
+</body>
+</html>
